f7b991a2ac0d13efef686a322fca3d0f99f2adc7
Dani Castilho <danicastilhodev@gmail.com>
2017-03-15 23:12:20 -0300

    iniciando templates

--- primeiroo-projeto/src/app/app.module.ts
+++ primeiroo-projeto/src/app/app.module.ts
@@ -3,20 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { CursosModule } from './cursos/cursos.module';
+
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
-import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component'
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MeuPrimeiroComponent,
-    MeuPrimeiro2Component
+    MeuPrimeiro2Component,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
