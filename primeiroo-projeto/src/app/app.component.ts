5797f113561e0e3d4db0d315df9b80c54641739a
Dani Castilho <daniele.castilho@yrbrands.com>
2017-03-21 17:30:23 -0300

    property binding e class binding

--- primeiroo-projeto/src/app/app.component.ts
+++ primeiroo-projeto/src/app/app.component.ts
@@ -4,10 +4,6 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @Component({
   selector: 'my-app',
-  template: `
-      <h1> Angular 2 Boilerplate </h1>
-      <p> Hello World! </p>
-  `,
-  directives: [DataBindingComponent]
+  templateUrl: './app.component.html'
 })
 export class AppComponent {}
